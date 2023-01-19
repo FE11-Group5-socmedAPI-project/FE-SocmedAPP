ac46fafcd3804d01ee0b19c8d8706eec2f0f0c40
Devi Andri Aldi <73923081+deviandrialdi@users.noreply.github.com>
2023-01-20 03:35:21 +0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useState, useMemo, useEffect } from "react";
-import { useDispatch } from "react-redux";
 
-import PageHome from "../pages/PageHome";
-import Register from "../pages/Register";
-import Login from "../pages/Login";
+//import { useDispatch } from "react-redux/es/exports";
+
 import Detail from "../pages/Detail";
 import Home from "../pages/Home";
 import Profil from "../pages/Profil";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import PageHome from "../pages/PageHome";
+import { ThemeContext } from "../utils/context";
 
 const router = createBrowserRouter([
   {
@@ -17,10 +19,6 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
   },
   {
     path: "/home",
@@ -34,9 +32,37 @@ const router = createBrowserRouter([
     path: "/profil",
     element: <Profil />,
   },
+   {
+    path: "/register",
+    element: <Register />,
+  },
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  //const dispatch = useDispatch();
+  const [theme, setTheme] = useState("light");
+  const background = useMemo(() => ({ theme, setTheme }), [theme]);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  useEffect(() => {
+    const getFavMovies = localStorage.getItem("FavMovie");
+    if (getFavMovies) {
+      //dispatch(setFavorites(JSON.parse(getFavMovies)));
+    }
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={background}>
+      <RouterProvider router={router} />
+    </ThemeContext.Provider>
+  );
 };
+
 export default App;
